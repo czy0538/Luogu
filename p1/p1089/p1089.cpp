9be93ab4de63f4717c863d09ea93831eb41e620d
czy0538 <czy0538@gmail.com>
2019-08-12 20:52:42 +0800

    2019年8月12日 20:52:41

--- p1/p1089/p1089.cpp
+++ p1/p1089/p1089.cpp
@@ -1,6 +1,27 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 int main()
 {
-    
+    int money = 0;
+    int mom = 0;
+    for (int i = 0; i < 12; ++i)
+    {
+        money += 300;
+        int temp;
+        cin >> temp;
+        money -= temp;
+        if (money < 0)
+        {
+            cout << -(i + 1) << endl;
+            return 0;
+        }
+        else
+        {
+            int t = money / 100;
+            money -= (t * 100);
+            mom += (t * 100);
+        }
+    }
+    cout << mom + mom * 0.2+money;
+    return 0;
 }
